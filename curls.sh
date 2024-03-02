9993f529da92ed46ae6ad85830d17a36c87e3369
dcorpi19ph <dcorpi16010@plataforma-utslp.net>
2024-03-01 18:34:53 -0600

    PRODUCTO P2 AID CORPI ZAVALA

--- curls.sh
+++ curls.sh
@@ -1,26 +1,38 @@
-#!bin/bash
+#!/bin/bash
 
-#Lista links a verificar
-
-links=(
-  "www.google.com"
-  "sito.utslp.edu.mx"
-  "www.plataforma-utslp.net"
-  "www.utslp.edu.mx"
-  "www.youtube.com"
-  "www.a.com"
-  "www.chat.openai.com"
-  "www.facebook.com"
+# Lista de URLs a verificar
+urls=(
+    "sito.utslp.edu.mx"
+    "www.google.com"
+    "www.plataforma-utslp.net"
+    "www.utslp.edu.mx"
+    "www.youtube.com"
+    "www.facebook.com"
+    "www.a.com.mx"
+    "www.utleon.edu.mx"
+    "www.itslp.edu.mx"
+    "www.nmnbcv.com"
 )
 
-echo "ok	|	fail"
-echo "----------|-----------"
+# Función para imprimir en verde
+print_ok() {
+    echo -e "\e[32mOK\e[0m"
+}
 
+# Función para imprimir en rojo
+print_fail() {
+    echo -e "\e[31mFail\e[0m"
+}
 
-for link in "${links[@]}"; do
-  if curl -s --head "$link" | head -n 1 | grep "HTTP/1.[01] 23 .." > /dev/null; then
-   echo "$link		|"
-  else
-   echo "|		$link"
-  fi
+echo "Página Web               | OK  | Fail"
+echo "-------------------------+-----+-----"
+
+# Recorre la lista de URLs y realiza solicitudes CURL
+for url in "${urls[@]}"; do
+    if curl -s --head "$url" | head -n 1 | grep "HTTP/1.[01] [23].." > /dev/null; then
+        echo -n "$url | $(print_ok)  |"
+    else
+        echo -n "$url |      | $(print_fail)"
+    fi
+    echo ""
 done
